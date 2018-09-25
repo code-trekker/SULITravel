64fbf3399e73199806927ea8feca70a10c4977fc
Roy Go Raposon Jr <roygoraposonjr@gmail.com>
2018-09-25 12:27:44 +0800

    added feedback

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -11,6 +11,9 @@ import { Feedback } from '../models/feedback';
 export class ProfileComponent implements OnInit {
     user: User;
     rating = 4.75;
+    input_rating:number;
+    input_comment:string;
+    
     feedbacks:Array<Feedback>;
     constructor(userService : UserService) {
         this.user = userService.user;
@@ -20,6 +23,17 @@ export class ProfileComponent implements OnInit {
         ]
      }
 
+     submit(){
+         console.log(this.input_rating,this.input_comment);
+         this.feedbacks.push(new Feedback(
+             this.user,this.input_rating,this.input_comment
+         ));
+         this.input_rating = 0;
+         this.input_comment = "";
+     }
+     onRatingChange($event){
+         this.input_rating =$event.rating;
+     }
     ngOnInit() {}
 
 }
